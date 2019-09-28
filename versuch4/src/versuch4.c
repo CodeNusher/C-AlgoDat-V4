d0fb02595f601ed8263d57f0950c48c9065428fa
Thomas Pongratz <pliss@gmx.de>
2019-09-29 00:49:06 +0200

    fehlerhafte Fallunterscheidung #2

--- versuch4/src/versuch4.c
+++ versuch4/src/versuch4.c
@@ -9,9 +9,14 @@
 
 
 #include <stdio.h>
-#define dt 1
+#define DT 1
+#define FLUCHTHOEHE 100
+#define F 500
+#define FAEHRE 1000
+#define FALLBESCH 1.6
 
 int main(void){
+	char ch = 'A';
 	int schubstufe;
 	float schub, a;
 	float halt = 50., valt = -10.;
@@ -20,37 +25,47 @@ int main(void){
 	printf("Mondlandung"
 			"\nBitte geben Sie jeweils eine Schubstufe von 0-9 an: ");
 	fflush(stdout);
-	while((scanf("%d", &schubstufe) > 0) && (schubstufe >= 0 && schubstufe <= 9)){
-		schub = 500 * schubstufe;
-		a = (schub / 1000) - 1.6;
-		printf("\nSchub: %d -> akt. Hoehe: %.1f m, Geschwindigkeit: %.1f m/s: ", schubstufe, halt, valt);
+	while((scanf("%d", &schubstufe) > 0) && (schubstufe >= 0 && schubstufe <= 9))
+	{
+		schub = F * schubstufe;
+		a = (schub / FAEHRE) - FALLBESCH;
 
-		vneu = valt + a * dt;
-		hneu = halt + vneu * dt;
+
+		vneu = valt + a * DT;
+		hneu = halt + vneu * DT;
+		printf("\nSchub: %d -> akt. Hoehe: %.1f m, Geschwindigkeit: %.1f m/s: ", schubstufe, halt, valt);
+		fflush(stdout);
 		valt = vneu;
 		halt = hneu;
-		if(hneu <= 100){
-
-			fflush(stdout);
-			if(hneu <= 0.1 && hneu >= 0){
-				if(vneu < 0.1){
-					printf("\nGewonnen!\n");
-				}
-				else{
-					printf("\nCRASH!\n");
-					fflush(stdout);
-				}
-			}
-			else if(hneu < 0.0){
-				printf("\nCrash!\n");
-				fflush(stdout);
-			}
-		}
-		else{
+		if(hneu > FLUCHTHOEHE)
+		{
 			printf("\nByeBye!\n");
 			fflush(stdout);
 		}
-	}
+		else
+		{
+			if(hneu < 0.1)
+			{
+				if(vneu < 0.1)
+				{
+					printf("\nGewonnen!\n");
+					fflush(stdout);
+				}
+				else
+				{
+					printf("\nCrash!\n");
+					fflush(stdout);
+				}
+			}
+		}
 
+	}
+	/* für Test Consolen-Abbruch */
+	printf("\nABBRUCH? ");
+	fflush(stdout);
+	while((scanf("%s", &ch)) > 0){
+		if(ch == 'j')
+			break;
+	}
 	return 0;
 }
